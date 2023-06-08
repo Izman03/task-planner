042aa4971f9050406ebfd4074ff08f64d40d85cd
Izman03 <116131757+Izman03@users.noreply.github.com>
2023-06-08 13:07:06 +0930

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Task class representing a task with various properties
 class Task {
   constructor(taskName, description, taskDate, assignedTo, taskStatus) {
     this.taskName = taskName;
@@ -8,86 +9,120 @@ class Task {
   }
 }
 
+// TaskPlanner class managing the tasks and rendering them
 class TaskPlanner {
   constructor() {
-    this.tasks = this.loadTasksFromStorage(); // Initialize tasks by loading from storage
+    // Load tasks from local storage when creating a new TaskPlanner instance
+    this.tasks = this.loadTasksFromStorage();
+    // Render the tasks on initialization
+    this.renderTasks();
   }
 
+  // Add a new task
   addTask(task) {
-    this.tasks.push(task); // Add task to the tasks array
-    this.renderTasks(); // Render the updated tasks
-    this.saveTasksToStorage(); // Save tasks to storage
+    this.tasks.push(task);
+    // Render the updated tasks
+    this.renderTasks();
+    // Save the tasks to local storage
+    this.saveTasksToStorage();
   }
 
+  // Delete a task based on its index
   deleteTask(index) {
-    this.tasks.splice(index, 1); // Remove task at the specified index from tasks array
-    this.renderTasks(); // Render the updated tasks
-    this.saveTasksToStorage(); // Save tasks to storage
+    this.tasks.splice(index, 1);
+    // Render the updated tasks
+    this.renderTasks();
+    // Save the tasks to local storage
+    this.saveTasksToStorage();
   }
 
+  // Toggle the completion status of a task
+  toggleTaskComplete(index) {
+    const task = this.tasks[index];
+    task.isComplete = !task.isComplete;
+    task.taskStatus = task.isComplete ? 'Done' : 'in-progress';
+    // Render the updated tasks
+    this.renderTasks();
+    // Save the tasks to local storage
+    this.saveTasksToStorage();
+  }
+
+  // Render the tasks on the page
   renderTasks() {
-    const taskContainer = document.getElementById("task-container"); // Get the task container element
-    taskContainer.innerHTML = ""; // Clear the existing content of the task container
+    const taskContainer = document.getElementById("task-container");
+    taskContainer.innerHTML = "";
 
     this.tasks.forEach((task, index) => {
-      const taskCard = document.createElement("div"); // Create a new div for the task card
-      taskCard.classList.add("col-md-4"); // Add Bootstrap class for styling
+      const taskCard = document.createElement("div");
+      taskCard.classList.add("col-md-4");
+      taskCard.style.outline = "none";
       taskCard.innerHTML = `
         <div class="card">
           <div class="card-body">
             <h5 class="card-title">${task.taskName}</h5>
-            <p class="card-text">${task.description}</p>
+            <hr>
+            <p class="card-text"><strong>Description:</strong> ${task.description}</p>
             <ul class="list-group list-group-flush">
               <li class="list-group-item"><strong>Due Date:</strong> ${task.taskDate}</li>
               <li class="list-group-item"><strong>Assigned To:</strong> ${task.assignedTo}</li>
-              <li class="list-group-item"><strong>Status:</strong> ${task.taskStatus}</li> 
+              <li class="list-group-item"><strong>Status:</strong> ${task.taskStatus}</li>
             </ul>
-            <button class="btn btn-danger" onclick="taskPlanner.deleteTask(${index})">Delete</button>
+            <button class="btn btn-danger" onclick="taskPlanner.deleteTask(${index})">❌</button>
+            ${task.taskStatus !== 'Done' ? `<button class="btn btn-success task-complete-button" onclick="taskPlanner.toggleTaskComplete(${index})" style="outline: none;">✔️</button>` : ''}
           </div>
         </div>
       `;
 
-      taskContainer.appendChild(taskCard); // Append the task card to the task container
+      taskContainer.appendChild(taskCard);
     });
   }
 
+  // Load tasks from local storage
   loadTasksFromStorage() {
-    const storedTasks = localStorage.getItem("tasks"); // Get tasks from local storage
-    return storedTasks ? JSON.parse(storedTasks) : []; // Parse and return tasks if present, otherwise return an empty array
+    const storedTasks = localStorage.getItem("tasks");
+    return storedTasks ? JSON.parse(storedTasks) : [];
   }
 
+  // Save tasks to local storage
   saveTasksToStorage() {
-    localStorage.setItem("tasks", JSON.stringify(this.tasks)); // Save tasks to local storage as a JSON string
+    localStorage.setItem("tasks", JSON.stringify(this.tasks));
   }
 }
 
-const taskPlanner = new TaskPlanner(); // Create a new instance of TaskPlanner
+// Create a new TaskPlanner instance
+const taskPlanner = new TaskPlanner();
 
+// Function to handle adding a new task
 function addTask(event) {
-  event.preventDefault(); // Prevent form submission from refreshing the page
+  event.preventDefault();
 
-  const taskNameInput = document.getElementById("task-name"); // Get the input element for task name
-  const descriptionInput = document.getElementById("description"); // Get the input element for task description
-  const taskDateInput = document.getElementById("task-date"); // Get the input element for task due date
-  const assignedToInput = document.getElementById("assigned-to"); // Get the input element for task assignee
-  const taskStatusInput = document.getElementById("task-status"); // Get the input element for task status
+  const taskNameInput = document.getElementById("task-name");
+  const descriptionInput = document.getElementById("description");
+  const taskDateInput = document.getElementById("task-date");
+  const assignedToInput = document.getElementById("assigned-to");
+  const taskStatusInput = document.getElementById("task-status");
 
+  // Create a new Task instance with input values
   const task = new Task(
     taskNameInput.value,
     descriptionInput.value,
     taskDateInput.value,
     assignedToInput.value,
     taskStatusInput.value
-  ); // Create a new Task object using input values
+  );
 
-  taskPlanner.addTask(task); // Add the task to the task planner
+  // Add the task to the TaskPlanner
+  taskPlanner.addTask(task);
 
-  taskNameInput.value = ""; // Clear the input value for task name
-  descriptionInput.value = ""; // Clear the input value for task description
-  taskDateInput.value = ""; // Clear the input value for task due date
-  assignedToInput.value = ""; // Clear the input value for task assignee
-  taskStatusInput.value = "To do"; // Reset the input value for task status
+  // Reset the input fields
+  taskNameInput.value = "";
+  descriptionInput.value = "";
+  taskDateInput.value = "";
+  assignedToInput.value = "";
+  taskStatusInput.value = "To do";
 }
 
-const taskForm = document.getElementById("task-form"); // Get the task form element
-taskForm.addEventListener("submit", addTask); // Add submit event listener to the task form
+// Add event listener to the task form submit button
+const taskForm = document.getElementById("task-form");
+taskForm.addEventListener("submit", addTask);
+
